76d74c569c0978efda87b7dd5960d6777f8e64b4
mansipandyaa29 <mansipandyaa29@gmail.com>
2024-05-07 17:51:59 -0700

    updated application.py

---
+++ application.py
@@ -0,0 +1,30 @@
+from flask import Flask,request,render_template
+
+# from sklearn.preprocessing import StandardScaler
+
+from src.predict.predict_pipeline import PredictPipeline
+
+application = Flask(__name__)
+app = application
+
+##Route for a home page
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata', methods=['GET','POST'])
+def predict_datapoint():
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        data = request.form.get('medical_history')
+        print(data)
+
+        predict_pipeline = PredictPipeline()
+        results = predict_pipeline.predict(data)
+
+        return render_template('home.html',results = results)
+
+if __name__ == "__main__":
+    app.run(threaded=True, host='0.0.0.0', port=5001)
