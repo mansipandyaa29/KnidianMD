a131b8c3e35355ae38472e9ebe7d13aba1b73fe5
mansipandyaa29 <mansipandyaa29@gmail.com>
2024-05-08 21:16:38 -0700

    updated application.py

--- application.py
+++ application.py
@@ -27,4 +27,4 @@ def predict_datapoint():
         return render_template('home.html',results = results)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug = True, host='0.0.0.0', port=5000)
